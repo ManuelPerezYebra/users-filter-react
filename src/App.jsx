f40c8cbd98e1a736d6e7c3d113d6743fcc97db29
Manuel <manuelperezyebra@gmail.com>
2024-03-05 09:25:58 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from "./GlobalStyles/GlobalStyles";
+
+const App = () => {
+	return <>
+	<GlobalStyles></GlobalStyles>
+	<h1>Núcleo de la aplicación</h1>;
+	
+	
+	</> 
+	
+};
+
+export default App;
